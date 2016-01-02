cf05ebb0d8f6d15ed5595fcbe5520f758999e3f8
postboy <anotherdiskmag@gmail.com>
2016-01-02 22:13:17 +0300

    code style changed

--- test.c
+++ test.c
@@ -10,144 +10,131 @@ License: BSD 2-Clause
 
 #include "lib/hdata.h"	//honeydata library
 
-void handleErrors(void)
+void error_handler(void)
 {
-  ERR_print_errors_fp(stderr);
-  abort();
+	ERR_print_errors_fp(stderr);
+	abort();
 }
 
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-  unsigned char *iv, unsigned char *ciphertext)
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv,
+	unsigned char *ciphertext)
 {
-  EVP_CIPHER_CTX *ctx;
+	EVP_CIPHER_CTX *ctx;
 
-  int len;
+	int len;
 
-  int ciphertext_len;
+	int ciphertext_len;
 
-  /* Create and initialise the context */
-  if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+	//create and initialise the context
+	if (!(ctx = EVP_CIPHER_CTX_new()))
+		error_handler();
 
-  /* Initialise the encryption operation. IMPORTANT - ensure you use a key
-   * and IV size appropriate for your cipher
-   * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-   * IV size for *most* modes is the same as the block size. For AES this
-   * is 128 bits */
-  if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-    handleErrors();
+	/*Initialise the encryption operation. IMPORTANT - ensure you use a key and IV size
+	appropriate for your cipher. In this example we are using 256 bit AES (i.e. a 256 bit key). The
+	IV size for *most* modes is the same as the block size. For AES this is 128 bits.*/
+	if (EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv) != 1)
+		error_handler();
 
-  /* Provide the message to be encrypted, and obtain the encrypted output.
-   * EVP_EncryptUpdate can be called multiple times if necessary
-   */
-  if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-    handleErrors();
-  ciphertext_len = len;
+	/*Provide the message to be encrypted, and obtain the encrypted output. EVP_EncryptUpdate can
+	be called multiple times if necessary.*/
+	if (EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len) != 1)
+		error_handler();
+	ciphertext_len = len;
 
-  /* Finalise the encryption. Further ciphertext bytes may be written at
-   * this stage.
-   */
-  if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
-  ciphertext_len += len;
+	//Finalise the encryption. Further ciphertext bytes may be written at this stage.
+	if (EVP_EncryptFinal_ex(ctx, ciphertext + len, &len) != 1)
+		error_handler();
+  	ciphertext_len += len;
 
-  /* Clean up */
-  EVP_CIPHER_CTX_free(ctx);
+	//clean up
+	EVP_CIPHER_CTX_free(ctx);
 
-  return ciphertext_len;
+	return ciphertext_len;
 }
 
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-  unsigned char *iv, unsigned char *plaintext)
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *iv,
+	unsigned char *plaintext)
 {
-  EVP_CIPHER_CTX *ctx;
+	EVP_CIPHER_CTX *ctx;
 
-  int len;
+	int len;
 
-  int plaintext_len;
+	int plaintext_len;
 
-  /* Create and initialise the context */
-  if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+	//create and initialise the context
+	if (!(ctx = EVP_CIPHER_CTX_new()))
+		error_handler();
 
-  /* Initialise the decryption operation. IMPORTANT - ensure you use a key
-   * and IV size appropriate for your cipher
-   * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-   * IV size for *most* modes is the same as the block size. For AES this
-   * is 128 bits */
-  if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-    handleErrors();
+	/*Initialise the decryption operation. IMPORTANT - ensure you use a key and IV size appropriate
+	for your cipher. In this example we are using 256 bit AES (i.e. a 256 bit key). The IV size for
+	*most* modes is the same as the block size. For AES this is 128 bits.*/
+	if (EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv) != 1)
+		error_handler();
 
-  /* Provide the message to be decrypted, and obtain the plaintext output.
-   * EVP_DecryptUpdate can be called multiple times if necessary
-   */
-  if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
-    handleErrors();
-  plaintext_len = len;
+	/*Provide the message to be decrypted, and obtain the plaintext output. EVP_DecryptUpdate can
+	be called multiple times if necessary.*/
+	if (EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len) != 1)
+		error_handler();
+	plaintext_len = len;
 
-  /* Finalise the decryption. Further plaintext bytes may be written at
-   * this stage.
-   */
-  if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) handleErrors();
-  plaintext_len += len;
+	//Finalise the decryption. Further plaintext bytes may be written at this stage.
+	if (EVP_DecryptFinal_ex(ctx, plaintext + len, &len) != 1)
+		error_handler();
+	plaintext_len += len;
 
-  /* Clean up */
-  EVP_CIPHER_CTX_free(ctx);
+	//clean up
+	EVP_CIPHER_CTX_free(ctx);
 
-  return plaintext_len;
+	return plaintext_len;
 }
 
 int main (void)
 {
-  /* Set up the key and iv. Do I need to say to not hard code these in a
-   * real application? :-)
-   */
+	//Set up the key and IV. Do I need to say to not hard code these in a real application? :-)
 
-  /* A 256 bit key */
-  unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
+	//a 256 bit key
+	unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
 
-  /* A 128 bit IV */
-  unsigned char *iv = (unsigned char *)"01234567890123456";
+	//a 128 bit IV
+	unsigned char *iv = (unsigned char *)"01234567890123456";
 
-  /* Message to be encrypted */
-  unsigned char *plaintext =
-                (unsigned char *)"The quick brown fox jumps over the lazy dog";
+	//message to be encrypted
+	unsigned char *plaintext = (unsigned char *)"The quick brown fox jumps over the lazy dog";
 
-  /* Buffer for ciphertext. Ensure the buffer is long enough for the
-   * ciphertext which may be longer than the plaintext, dependant on the
-   * algorithm and mode
-   */
-  unsigned char ciphertext[128];
+	/*Buffer for ciphertext. Ensure the buffer is long enough for the ciphertext which may be
+	longer than the plaintext, dependant on the algorithm and mode*/
+	unsigned char ciphertext[128];
 
-  /* Buffer for the decrypted text */
-  unsigned char decryptedtext[128];
+	//buffer for the decrypted text
+	unsigned char decryptedtext[128];
 
-  int decryptedtext_len, ciphertext_len;
+	int decryptedtext_len, ciphertext_len;
 
-  /* Initialise the library */
-  ERR_load_crypto_strings();
-  OpenSSL_add_all_algorithms();
-  OPENSSL_config(NULL);
+	//initialise the library
+	ERR_load_crypto_strings();
+	OpenSSL_add_all_algorithms();
+	OPENSSL_config(NULL);
 
-  /* Encrypt the plaintext */
-  ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
-                            ciphertext);
+	//encrypt the plaintext
+	ciphertext_len = encrypt(plaintext, strlen ((char *)plaintext), key, iv, ciphertext);
 
-  /* Do something useful with the ciphertext here */
-  printf("Ciphertext is:\n");
-  BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
+	//do something useful with the ciphertext here
+	printf("Ciphertext is:\n");
+	BIO_dump_fp(stdout, (const char *)ciphertext, ciphertext_len);
 
-  /* Decrypt the ciphertext */
-  decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
-    decryptedtext);
+	//decrypt the ciphertext
+	decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv, decryptedtext);
 
-  /* Add a NULL terminator. We are expecting printable text */
-  decryptedtext[decryptedtext_len] = '\0';
+	//add a NULL terminator, because we are expecting printable text
+	decryptedtext[decryptedtext_len] = '\0';
 
-  /* Show the decrypted text */
-  printf("Decrypted text is:\n");
-  printf("%s\n", decryptedtext);
+	//show the decrypted text
+	printf("Decrypted text is:\n");
+	printf("%s\n", decryptedtext);
 
-  /* Clean up */
-  EVP_cleanup();
-  ERR_free_strings();
+	//clean up
+	EVP_cleanup();
+	ERR_free_strings();
 
-  return 0;
+	return 0;
 }
